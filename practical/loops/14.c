d0ce42ac6bf6e0f112f9494f7cc20453de61f194
Yug <yug02soni@gmail.com>
2026-04-30 13:38:18 +0530

    update code

---
+++ practical/loops/14.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main(int argc, char const *argv[])
+{
+    int i, j;
+    for (i = 0; i <= 5; i++)
+    {
+    
+        for (j = 0; j <= i; j++)
+        {
+            printf("%d", j);
+        }
+        printf(" \n ");
+    }
+    
+    return 0;
+}
