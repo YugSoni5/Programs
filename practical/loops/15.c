a6ac088e622e40c5ff706edad555073b3b2fd6b3
Yug <yug02soni@gmail.com>
2026-04-30 13:43:08 +0530

    update source code

--- practical/loops/15.c
+++ practical/loops/15.c
@@ -1,1 +1,24 @@
-#include<
+#include <stdio.h>
+
+int main()
+{
+    int rows = 5; // Number of lines to print
+
+    for (int i = 0; i < rows; i++)
+    {
+        // First part: Decreasing from i to 0
+        for (int j = i; j >= 0; j--)
+        {
+            printf("%d", j);
+        }
+        // Second part: Increasing from 1 to i
+        for (int j = 1; j <= i; j++)
+        {
+            printf("%d", j);
+        }
+
+        printf("\n"); // Move to the next line
+    }
+
+    return 0;
+}
